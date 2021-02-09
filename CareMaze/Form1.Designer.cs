7eac0731e20e08fe74a6a293972842877bcfcc17
Abolfazl <dahaghaynhmh@gmail.com>
2021-02-09 19:25:09 +0330

    Signed-off-by: Abolfazl <dahaghaynhmh@gmail.com>

--- CareMaze/Form1.Designer.cs
+++ CareMaze/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -42,12 +43,23 @@
             this.label2.UseCompatibleTextRendering = true;
             this.label2.UseWaitCursor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(695, 678);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSlateBlue;
             this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
@@ -62,6 +74,7 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CareMaze/Form1.cs
+++ CareMaze/Form1.cs
@@ -18,16 +18,28 @@ namespace CareMaze
         int width = 40*13 + 9;//number---width of each pixel---first x
         private void Form1_Load(object sender, EventArgs e)
         {
-            for(int x=9;x <=width; x += 13)
+            Random rnd = new Random();
+            int rand_start_x = rnd.Next(0, 40)*13 + 9;
+            int rand_start_y = rnd.Next(0, 40)*13 + 12;
+            int rand_end_x = rnd.Next(0, 40)*13 + 9;
+            int rand_end_y = rnd.Next(0, 40)*13 + 12;
+            for (int x=9;x <=width; x += 13)
             {
                 for(int y=12;y<=high; y += 13)
                 {
                     Label lbl = new Label();
                     lbl.Location = new Point(x,y);
                     lbl.Size = new Size(13, 13);
-                    lbl.BackColor = Color.Green;
-                    lbl.ForeColor = Color.Brown;
+                    lbl.BackColor = Color.White;
                     lbl.BorderStyle = BorderStyle.FixedSingle;
+                    if(rand_start_x == x && rand_start_y == y)
+                    {
+                        lbl.BackColor = Color.LightGreen;
+                    }
+                    if (rand_end_x == x && rand_end_y == y)
+                    {
+                        lbl.BackColor = Color.Red;
+                    }
                     this.Controls.Add(lbl);
                 }
             }
