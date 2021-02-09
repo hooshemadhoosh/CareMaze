b26eb6c26876608de6bb17dc88f9ae2f81f36ca6
Abolfazl <dahaghaynhmh@gmail.com>
2021-02-09 15:15:44 +0330

    Signed-off-by: Abolfazl <dahaghaynhmh@gmail.com>

--- CareMaze/Form1.Designer.cs
+++ CareMaze/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(732, 703);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "CareMaze";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
